583a38806f78a22a052d30cea9ec2038fad1ec84
sqirum <pauloslh94@gmail.com>
2022-09-18 12:55:45 +0100

    add input js validation

--- app.js
+++ app.js
@@ -2,3 +2,21 @@
 
 //console.table(obj);
 //console.dir(obj);
+
+let input = document.getElementById('email-input');
+let feedbackMsg = document.getElementById('feedback-message');
+let btn = document.getElementById('submit-btn');
+
+
+btn.addEventListener('click', inputValidation);
+function inputValidation() {
+    if (input.value === '') {
+        feedbackMsg.innerText = 'Field is empty.'
+        feedbackMsg.style.display = 'block';
+    } else {
+        if (!(input.value.includes('@'))) {
+            feedbackMsg.innerText = 'Please provide a valid email.'
+            feedbackMsg.style.display = 'block';
+        }
+    }
+}
